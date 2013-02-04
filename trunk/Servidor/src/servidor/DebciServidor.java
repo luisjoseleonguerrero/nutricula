e8623da0eb958a1551943e9c1623da8f298b1fb4
andoni94@gmail.com <andoni94@gmail.com@f5df0eb5-48de-7b0f-4b39-e7a380560976>
2013-02-04 07:10:56 +0000

    6th update of the nutricula release. Still hacking!

--- trunk/Servidor/src/servidor/DebciServidor.java
+++ trunk/Servidor/src/servidor/DebciServidor.java
@@ -6,6 +6,7 @@ package servidor;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -240,6 +241,8 @@ public class DebciServidor extends javax.swing.JFrame {
                         
                 }
             });  
+            File miDir = new File (".");
+            System.out.println(miDir.getCanonicalPath()+miDir.getAbsolutePath());
             ProcesoServidor();
     }
     static void ProcesoServidor() throws IOException{
